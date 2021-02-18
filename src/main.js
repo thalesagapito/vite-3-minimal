1ec2c0c99d4edd3dd7b8c3c37cf92af4d1e1d9f0
Thales Agapito <thsousa101@gmail.com>
2021-02-18 18:44:23 -0300

    Remove axios

--- src/main.js
+++ src/main.js
@@ -1,19 +1,15 @@
 import { createSSRApp } from 'vue'
 import { metaManager } from './plugins/vue-meta';
 import { createRouter } from './plugins/router';
-import { axios } from './plugins/axios';
 import App from './App.vue'
 import './index.css';
-
-
 
 export function createApp() {
   const app = createSSRApp(App);
   const router = createRouter();
   app
     .use(router)
-    .use(metaManager)
-    .use(axios);
+    .use(metaManager);
     
   return { app, router };
 }
